0cb0fb3d859a249d3ea837e32ad57c6dc1eab761
Georg Dangl <georg@georgdangl.de>
2016-05-08 18:04:14 +0200

    Add support for dynamic client registrations and changed implementation from hardcoded server endpoints to dynamic endpoints for authorization

--- BCF-API/Program.cs
+++ BCF-API/Program.cs
@@ -7,53 +7,86 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace BCF_API
 {
-  static class Program
-  {
-    /// <summary>
-    /// The main entry point for the application.
-    /// </summary>
-    [STAThread]
-    static void Main()
+    static class Program
     {
-      // Please fill inn:
-      string url = "https://bim--it.net/bcf";
-      string clientId = "YOUR_CLIENT_ID";
-      string clientSecret = "YOU_CLIENT_SECRET";
+        public const bool USE_DYNAMIC_CLIENT_REGISTRATION = true;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Please fill inn:
+            string url = "https://bim--it.net/bcf";
+            string clientId;
+            string clientSecret;
+
+            var client = new WebWrapper();
 
 
-      var login = new ServerLoginUI();
-      OAuthModule.ServerLoginUIObj = login;
-      login.ShowDialog();
+            // Get server authentication endpoints
+            dynamic authResult = JObject.Parse(client.Request(url + "/auth", "GET", string.Empty, string.Empty));
+            // Get user-client grant authorization endpoint (Webpage that is displayed to the user)
+            var authEndpoint = (string) authResult.oauth2_auth_url;
+            // Get endpoint for dynamic client registrations
+            var clientRegistrationEndpoint = (string) authResult.oauth2_dynamic_client_reg_url;
+            // Get the endpoint where token should be POSTed to
+            var tokenEndpoint = (string) authResult.oauth2_token_url;
 
-      var client = new WebWrapper();
+
+            if (USE_DYNAMIC_CLIENT_REGISTRATION)
+            {
+                var clientInformation = new
+                {
+                    client_name = "BCF API Example Client",
+                    client_description = "Simple example client",
+                    client_url = "https://github.com/rvestvik/BcfApiExampleClient",
+                    redirect_url = "http://localhost:25512" // IP adress on which the local Kayak Server is listening
+                };
+                // Register an OAuth2 client
+                dynamic clientCreationResult = JObject.Parse(client.Request(clientRegistrationEndpoint, "POST", string.Empty, JsonConvert.SerializeObject(clientInformation)));
+                clientId = (string) clientCreationResult.client_id;
+                clientSecret = (string) clientCreationResult.client_secret;
+            }
+            else
+            {
+                clientId = "YOUR_CLIENT_ID";
+                clientSecret = "YOU_CLIENT_SECRET";
+            }
 
 
-      string AuthorizationPlain = clientId + ":" + clientSecret;
-      string AuthorizationEncoded = Convert.ToBase64String(Encoding.Default.GetBytes(AuthorizationPlain));
-      string result = client.Request(url+"/OAuth2/token", "POST", "Basic " + AuthorizationEncoded, "grant_type=authorization_code&code=" + login.sAuthCode + "\n");
-      dynamic json = JObject.Parse(result);
-      string token = json.access_token;
+            var login = new ServerLoginUI($"{authEndpoint}?response_type=code&client_id={clientId}&state={Guid.NewGuid()}");
+            OAuthModule.ServerLoginUIObj = login;
+            login.ShowDialog();
 
-      var str = new StringBuilder();
-      str.AppendLine("access_token: " + json.access_token);
-      str.AppendLine("token_type: " + json.token_type);
-      str.AppendLine("expires_in: " + json.expires_in);
-      str.AppendLine("refresh_token: " + json.refresh_token);
-      MessageBox.Show(str.ToString());
+            string AuthorizationPlain = clientId + ":" + clientSecret;
+            string AuthorizationEncoded = Convert.ToBase64String(Encoding.UTF8.GetBytes(AuthorizationPlain));
+            string result = client.Request(tokenEndpoint, "POST", "Basic " + AuthorizationEncoded, "grant_type=authorization_code&code=" + login.sAuthCode);
+            dynamic json = JObject.Parse(result);
+            string token = json.access_token;
 
-      result = client.Request(url+"/1.0/projects", "GET", "Bearer " + token, "");
-      dynamic projects = JObject.Parse("{list:" + result + "}");
+            var str = new StringBuilder();
+            str.AppendLine("access_token: " + json.access_token);
+            str.AppendLine("token_type: " + json.token_type);
+            str.AppendLine("expires_in: " + json.expires_in);
+            str.AppendLine("refresh_token: " + json.refresh_token);
+            MessageBox.Show(str.ToString());
 
-      StringBuilder
-        sb = new StringBuilder();
-      foreach (var project in projects.list)
-      {
-        sb.AppendLine((string)project.name);
-      }
-      MessageBox.Show("GET https://bim--it.net/bcf/1.0/projects" + Environment.NewLine + Environment.NewLine + sb.ToString());
+            result = client.Request(url + "/1.0/projects", "GET", "Bearer " + token, "");
+            dynamic projects = JObject.Parse("{list:" + result + "}");
+
+            StringBuilder
+                sb = new StringBuilder();
+            foreach (var project in projects.list)
+            {
+                sb.AppendLine((string) project.name);
+            }
+            MessageBox.Show("GET https://bim--it.net/bcf/1.0/projects" + Environment.NewLine + Environment.NewLine + sb.ToString());
+        }
     }
-  }
 }
